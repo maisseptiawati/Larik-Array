f55728efcca6ee78e4d5a0da373a1b0424a8dde0
maisseptiawati <septiawatimais@gmail.com>
2016-04-01 17:54:30 +0700

    tugas

---
+++ Array3.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array3;
+
+/**
+ *
+ * @author Ma'is
+ */
+public class Array3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    String [] a = {"sepeda","motor","mobil", "becak","bus","helikopter","pesawat", "kapal","bajaj","taksi"};
+    String [] b = {"anggur","apel","blueberry","mangga","nanas","pir","cery","semangka","salak", "rambutan"}; 
+    //int [] c = {1 , 2 ,3 ,4, 5, 6, 7, 8,9,10}; 
+    
+    System.out.println ("1." + a[0]);
+    System.out.println ("2." + a[1]);
+    System.out.println ("3." + a[2]);
+    System.out.println ("4." + a[3]);
+    System.out.println ("5." + a[4]);
+    System.out.println ("6." + a[5]);
+    System.out.println ("7." + a[6]);
+    System.out.println ("8." + a[7]);
+    System.out.println ("9." + a[8]);
+    System.out.println ("10." + a[9]);
+    
+    System.out.println ("1." + b[0]);
+    System.out.println ("2." + b[1]);
+    System.out.println ("3." + b[2]);
+    System.out.println ("4." + b[3]);
+    System.out.println ("5." + b[4]);
+    System.out.println ("6." + b[5]);
+    System.out.println ("7." + b[6]);
+    System.out.println ("8." + b[7]);
+    System.out.println ("9." + b[8]);
+    System.out.println ("10." +b[9]);
+    
+    
+   
+   
+    
+    // TODO code application logic here
+    }
+    
+}
+ 
