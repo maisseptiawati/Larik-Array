3759f81cd3c66f226d373d4021293b8d0d03e850
maisseptiawati <septiawatimais@gmail.com>
2016-04-01 17:52:26 +0700

    tugas

---
+++ Array2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array2;
+
+/**
+ *
+ * @author Ma'is
+ */
+public class Array2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int[] a = {100,200,300,400};
+    String [] b = {"anggur","apel","blueberry","mangga"}; 
+    
+    System.out.println (a[0]);
+    System.out.println (a[1]);
+    System.out.println (a[2]);
+    System.out.println (a[3]);
+    
+    System.out.println (b[0]);
+    System.out.println (b[1]);
+    System.out.println (b[2]);
+    System.out.println (b[3]);
+    }
+    
+}
