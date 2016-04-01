53372365978a6cb187e6d918f8422f06c8e94826
maisseptiawati <septiawatimais@gmail.com>
2016-04-01 17:49:41 +0700

    tugas

---
+++ Array.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array;
+
+/**
+ *
+ * @author Ma'is
+ */
+public class Array {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        String [] b = new String [10];        
+        
+        b[0]="1. apel";
+        b[1]="2. jeruk";
+        b[2]="3. manggis";
+        b[3]="4. cery";
+        b[4]="5. anggur";
+        b[5]="6. durian";
+        b[6]="7. semangka";
+        b[7]="8. melon";
+        b[8]="9. mangga";  
+        b[9]="10.pir";
+        
+     
+        System.out.println(b[0]);
+        System.out.println(b[1]);
+        System.out.println(b[2]);
+        System.out.println(b[3]);
+        System.out.println(b[4]);
+        System.out.println(b[5]);
+        System.out.println(b[6]);
+        System.out.println(b[7]);
+        System.out.println(b[8]);
+        System.out.println(b[9]);
+    }
+    
+}
